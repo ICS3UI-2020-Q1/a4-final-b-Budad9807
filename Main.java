374b73f4ff584ef405e8445ca64e23fc0ee245be
Budad9807 <Budad9807@wrdsb.ca>
2020-10-14 16:00:12 +0000

    Made a program that will take 10% off

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Taking a number and taking sum of it
+ * @author David
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // make a scanner for user inpit
+    Scanner input = new Scanner(System.in);
+    // ask the user for a positive integer
+    System.out.println("Please enter a positive integer to add the digits of");
+   // make a storage for the integer the user entered
+    int num = input.nextInt();
+    // make num equal to zero
+    int total = 0;
+    while(num != 0){
+      total += num%10;
+      // make the calculation for the total
+      num = (int)num / 10;
+    }
+    // make it print out to the screen with the total
+    System.out.println("The sum of these digits is" + total);
     
   }
 }
